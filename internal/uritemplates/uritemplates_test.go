3c01ddd61cd0c0183c23545896cb7cd68073053c
Gary Mann <garythemann@users.noreply.github.com>
2023-03-13 21:52:49 -0700

    Unit tests for internal URI templates

--- internal/uritemplates/uritemplates_test.go
+++ internal/uritemplates/uritemplates_test.go
@@ -5,7 +5,89 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
 )
+
+var _ = Describe("Names", func() {
+
+	DescribeTable("examples", func(template string, expected []string) {
+		tpl, _ := uritemplates.Parse(template)
+		names := tpl.Names()
+		Expect(names).To(Equal(expected))
+	},
+		Entry("struct",
+			"{/path*,Version}{?opts*}",
+			[]string{"path", "Version", "opts"},
+		), Entry("Pointer to struct:",
+			"{?opts*}",
+			[]string{"opts"},
+		), Entry("Map expansion cannot be truncated:",
+			"{?opts:3}",
+			[]string{"opts"},
+		), Entry("Map whose values are not all strings:",
+			"{?one*}",
+			[]string{"one"},
+		), Entry("Value of inappropriate type:",
+			"{?one*}",
+			[]string{"one"},
+		), Entry("Truncated array whose values are not all strings:",
+			"{?one:3}",
+			[]string{"one"},
+		),
+	)
+})
+
+var _ = Describe("Expand", func() {
+
+	DescribeTable("examples", func(v any, template string, expected string) {
+		tpl, _ := uritemplates.Parse(template)
+		expanded, _ := tpl.Expand(v)
+		Expect(expanded).To(Equal(expected))
+	},
+		Entry("struct",
+			Location{
+				Path:    []any{"main", "quux"},
+				Version: 2,
+				Opts: Options{
+					Format: "pdf",
+				},
+			},
+			"{/path*,Version}{?opts*}",
+			"/main/quux/2?fmt=pdf",
+		), Entry("Pointer to struct:",
+			&Location{Opts: Options{Format: "pdf"}},
+			"{?opts*}",
+			"?fmt=pdf",
+		), Entry("Map expansion cannot be truncated:",
+			Location{Opts: Options{Format: "pdf"}},
+			"{?opts:3}",
+			"",
+		), Entry("Map whose values are not all strings:",
+			map[string]any{
+				"one": map[string]any{
+					"two": 42,
+				},
+			},
+			"{?one*}",
+			"?two=42",
+		), Entry("Value of inappropriate type:",
+			42,
+			"{?one*}",
+			"",
+		), Entry("Truncated array whose values are not all strings:",
+			map[string]any{"one": []any{1234}},
+			"{?one:3}",
+			"?one=123",
+		),
+	)
+
+	DescribeTable("errors", func(template string, expected types.GomegaMatcher) {
+		_, err := uritemplates.Parse(template)
+		Expect(err).To(expected)
+	},
+		Entry("too many colons", "{opts:1:2}", HaveOccurred()),
+	)
+})
 
 var _ = Describe("UriTemplate", func() {
 
@@ -39,3 +121,13 @@ var _ = Describe("UriTemplate", func() {
 	})
 
 })
+
+type Location struct {
+	Path    []any   `uri:"path"`
+	Version int     `json:"version"`
+	Opts    Options `opts`
+}
+
+type Options struct {
+	Format string `uri:"fmt"`
+}
