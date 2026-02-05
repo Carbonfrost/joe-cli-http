2caffa64d627fd4ce6b0412eba2c459bb21cdb72
Gary Mann <garythemann@users.noreply.github.com>
2026-02-04 22:01:23 -0800

    Fix copyright headers

--- internal/uritemplates/uritemplates.go
+++ internal/uritemplates/uritemplates.go
@@ -1,8 +1,5 @@
-// Copyright 2025 The Joe-cli Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
 // Copyright 2013 Joshua Tacoma. All rights reserved.
-// Copyright 2023 Joe-cli-http authors.
+// Copyright 2023-2025 The Joe-cli Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
