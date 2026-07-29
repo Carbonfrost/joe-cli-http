adca4b4fb4385f91d7dc12c4b18dc3b54c0f7691
Gary Mann <garythemann@users.noreply.github.com>
2026-07-28 21:48:51 -0700

    NewRecoveryMiddleware

--- httpserver/middleware.go
+++ httpserver/middleware.go
@@ -1,8 +1,11 @@
 package httpserver
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"runtime/debug"
 
 	"github.com/Carbonfrost/joe-cli/extensions/expr/expander"
 )
@@ -27,4 +30,32 @@ func NewHeaderMiddleware(name, value string) func(http.Handler) http.Handler {
 			inner.ServeHTTP(w, r)
 		})
 	}
+}
+
+// NewRecoveryMiddleware is middleware that recovers from panics, logs the panic and
+// backtrace, and writes out a HTTP 500 (Internal Server Error) status if
+// appropriate.
+func NewRecoveryMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if rvr := recover(); rvr != nil {
+				if rvr == http.ErrAbortHandler {
+					// we don't recover http.ErrAbortHandler so the response
+					// to the client is aborted, this should not be logged
+					panic(rvr)
+				}
+				printDebugStack()
+
+				if r.Header.Get("Connection") != "Upgrade" {
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+func printDebugStack() {
+	debugStack := string(debug.Stack())
+	fmt.Fprintln(os.Stderr, debugStack)
 }
